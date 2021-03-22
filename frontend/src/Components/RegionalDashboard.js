700954aeafed6121022e571a90479a6f8d51800b
Jeffin <jeffinsiby@gmail.com>
2021-03-22 13:49:26 +0000

    RegionalDashboard colours changeable from colourCode
    Implemented react refs to change the progress bar colour from the app components using colourCode variable.
    
    Hardcoded colour values removed from CSS

--- frontend/src/Components/RegionalDashboard.js
+++ frontend/src/Components/RegionalDashboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import FadeIn from 'react-fade-in';
 import TwitterLogoList from './TwitterLogoList';
 import ProgressBar from 'react-bootstrap/ProgressBar';
@@ -13,7 +13,35 @@ import sadness_emoji from "../images/1F622.svg";
 
 
 export default class RegionalDashboard extends Component {
-    
+    constructor(props){
+        super(props);
+        this.fear_inner = createRef();
+        this.confident_inner = createRef();
+        this.anger_inner = createRef();
+        this.joy_inner = createRef();
+        this.sadness_inner = createRef();
+    }
+    componentDidMount(){
+        let {colourCode} = this.props;
+
+        // Changes the colour of the progress bar by accessing the
+        // child element
+        const chng_inner_color = (ref, colour) => {
+            if (ref.current) {
+                const inner = ref.current.querySelector(".progress-bar");
+                if ( inner ) {
+                   inner.style.backgroundColor = colour;
+                }
+            }
+        }
+
+        chng_inner_color(this.fear_inner, colourCode.Fear);
+        chng_inner_color(this.confident_inner, colourCode.Confident);
+        chng_inner_color(this.anger_inner, colourCode.Anger);
+        chng_inner_color(this.joy_inner, colourCode.Joy);
+        chng_inner_color(this.sadness_inner, colourCode.Sadness);
+    }
+
     render() {
         let {data} = this.props;
         // prevent crashing if data hasn't been loaded in
@@ -92,11 +120,11 @@ export default class RegionalDashboard extends Component {
                                 Emotional Breakdown
                             </div>
                             <div className="progress_bar_container">
-                                <ProgressBar animated now={data.fear} label={Math.round(data.fear)+"%"} className="red-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px", "borderRadius": "19.5px"}}/>
-                                <ProgressBar animated now={data.confident} label={Math.round(data.confident)+"%"} className="green-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px", "borderRadius": "19.5px"}}/>
-                                <ProgressBar animated now={data.anger} label={Math.round(data.anger)+"%"} className="purple-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px", "borderRadius": "19.5px" }}/>
-                                <ProgressBar animated now={data.joy} label={Math.round(data.joy)+"%"} className="orange-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px" , "borderRadius": "19.5px"}}/>
-                                <ProgressBar animated now={data.sadness} label={Math.round(data.sadness)+"%"} className="blue-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px", "borderRadius": "19.5px" }}/>
+                                <ProgressBar ref={this.fear_inner} animated now={data.fear} label={Math.round(data.fear)+"%"} className="styled-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px", "borderRadius": "19.5px"}}/>
+                                <ProgressBar ref={this.confident_inner} animated now={data.confident} label={Math.round(data.confident)+"%"} className="styled-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px", "borderRadius": "19.5px"}}/>
+                                <ProgressBar ref={this.anger_inner} animated now={data.anger} label={Math.round(data.anger)+"%"} className="styled-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px", "borderRadius": "19.5px" }}/>
+                                <ProgressBar ref={this.joy_inner} animated now={data.joy} label={Math.round(data.joy)+"%"} className="styled-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px" , "borderRadius": "19.5px"}}/>
+                                <ProgressBar ref={this.sadness_inner} animated now={data.sadness} label={Math.round(data.sadness)+"%"} className="styled-progress-bar" height="1px" style={{ height: "30.82px", margin:"10px 0px 10px 0px", "borderRadius": "19.5px" }}/>
                             </div>
                         </div>
                         <div className="sample-size">
