e440d9c42cc5038056468a1034fb111c066d039a
boyla950 <matthew.i.boyle@durham.ac.uk>
2021-03-25 12:33:34 +0000

    backend updated to make analysis run on startup

--- backend/server.js
+++ backend/server.js
@@ -8,50 +8,59 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const uri = "mongodb+srv://dbUser:oEN8wfsUMgPmQkut@cluster0.7xljy.mongodb.net/emotional-map?retryWrites=true&w=majority"
-const client = new MongoClient(uri, { useUnifiedTopology: true });
-
 require('dotenv').config()
+
+const uri = `${process.env.MONGO_URI}`
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 
 app.use(express.json());
 
 let data_list = []
 
+//loads in list of regions
 const regions_list = JSON.parse(fs.readFileSync('regions.json'))
 
+//gets the tweet data for specific region
 async function get_tweets(region) {
-
+    //authentication for Twitter API
     const headers = { 'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}` }
 
+    //sets parameters for API Call
     location = `${region.lat},${region.lng},${region.r}mi`
-    console.log(location)
 
     tweet_json = {
         "region_name": region.name,
         "tweets": []
     }
-
-    for(let j = 0; j < 3; j++){
+    //makes the API call 3 times in order get upto 300 tweets (max allowed 100 per call)
+    for (let j = 0; j < 3; j++) {
 
         let res = await fetch(`https://api.twitter.com/1.1/search/tweets.json?q=&geocode=${location}&count=100&tweet_mode=extended`, { method: 'GET', headers: headers })
+
+        if (res.status !== 200) {
+
+            console.log(res)
+
+        }
 
         const city_json = await res.json()
 
         for (let i = 0; i < await city_json.statuses.length; i++) {
+            //filters out retweets as dont show full text
             if (typeof city_json.statuses[i].retweeted_status === "undefined") {
                 tweet_json.tweets.push(await city_json.statuses[i].full_text)
             }
 
         }
-    }   
-
-    console.log(tweet_json.tweets.length)
+    }
+    //adds regional tweet data to data list
     data_list.push(tweet_json)
 
     return
 
 }
 
+// loops through each region and passes it to get_tweets to update region tweet data
 async function get_region_tweets() {
 
     for (let i = 0; i < regions_list.length; i++) {
@@ -62,16 +71,25 @@ async function get_region_tweets() {
 
 }
 
+//gets trends for a region
 async function get_trend(woeid) {
 
+     //authentication for Twitter API
     const headers = { 'Authorization': `Bearer ${process.env.TWITTER_BEARER_TOKEN}` }
 
     let res = await fetch(`https://api.twitter.com/1.1/trends/place.json?id=${woeid}`, { method: 'GET', headers: headers })
+
+    if (res.status !== 200) {
+
+        console.log(res)
+
+    }
 
     const trend_json = await res.json()
 
     let trend_list = [];
 
+    //filters list for top 5 trends
     for (let i = 0; i < trend_json[0].trends.length; i++) {
 
         if (trend_list.length < 5) {
@@ -85,6 +103,7 @@ async function get_trend(woeid) {
     return trend_list
 }
 
+// takes analysis and returns sorted list of 5 cities with greatest joy score
 function get_happiest(region_scores) {
 
     let copy = region_scores.slice(0);
@@ -98,6 +117,7 @@ function get_happiest(region_scores) {
 
 }
 
+//analyses tweet data for all regions
 async function analyse_tweets() {
 
     uk_score = region_score = {
@@ -127,6 +147,7 @@ async function analyse_tweets() {
 
     region_analysis = []
 
+    //iterates through each region
     for (let i = 0; i < data_list.length; i++) {
 
         region_score = {
@@ -147,11 +168,9 @@ async function analyse_tweets() {
         fear_count = 0
         sadness_count = 0
         confident_count = 0
+        text = { "text": data_list[i].tweets.join(' \n ') }
 
-        text = { "text": data_list[i].tweets.join() }
-        console.log(process.env.WATSON_TONE_URL)
-        console.log(process.env.WATSON_TONE_API_KEY)
-
+        //sends tweet data for to region to watson for analysis as one large doc
         let res = await fetch(`${process.env.WATSON_TONE_URL}/v3/tone?version=2017-09-21`, {
             method: 'POST',
             headers: {
@@ -161,9 +180,15 @@ async function analyse_tweets() {
             body: JSON.stringify(text)
         })
 
+        if (res.status !== 200) {
+
+            console.log(res)
+
+        }
+
         let analysis = await res.json()
 
-        console.log(analysis)
+        //iterates through each 'sentence' (determined by watson) and tallies emotion values for region and UK
 
         if (typeof (analysis.sentences_tone) !== 'undefined') {
             for (let j = 0; j < analysis.sentences_tone.length; j++) {
@@ -201,34 +226,39 @@ async function analyse_tweets() {
                 }
 
             }
-        
 
-        region_score.sample_size += analysis.sentences_tone.length
-        uk_score.sample_size += analysis.sentences_tone.length
+
+            region_score.sample_size += analysis.sentences_tone.length
+            uk_score.sample_size += analysis.sentences_tone.length
         }
 
+        //calculates percentage of each emot in region
         region_score.joy = (joy_count) / (joy_count + anger_count + fear_count + sadness_count + confident_count) * 100
         region_score.anger = (anger_count) / (joy_count + anger_count + fear_count + sadness_count + confident_count) * 100
         region_score.fear = (fear_count) / (joy_count + anger_count + fear_count + sadness_count + confident_count) * 100
         region_score.sadness = (sadness_count) / (joy_count + anger_count + fear_count + sadness_count + confident_count) * 100
         region_score.confident = (confident_count) / (joy_count + anger_count + fear_count + sadness_count + confident_count) * 100
 
+        //gets list of trends for region
         trend_list = await get_trend(regions_list[i].woeid)
 
         region_score.trend1 = trend_list[0]
         region_score.trend2 = trend_list[1]
         region_score.trend3 = trend_list[2]
 
+        //adds region analysis JSON object to analysis list
         region_analysis.push(region_score)
 
     }
 
+    //calculates percentage of each emot in region
     uk_score.joy = (uk_joy_count) / (uk_joy_count + uk_anger_count + uk_fear_count + uk_sadness_count + uk_confident_count) * 100
     uk_score.anger = (uk_anger_count) / (uk_joy_count + uk_anger_count + uk_fear_count + uk_sadness_count + uk_confident_count) * 100
     uk_score.fear = (uk_fear_count) / (uk_joy_count + uk_anger_count + uk_fear_count + uk_sadness_count + uk_confident_count) * 100
     uk_score.sadness = (uk_sadness_count) / (uk_joy_count + uk_anger_count + uk_fear_count + uk_sadness_count + uk_confident_count) * 100
     uk_score.confident = (uk_confident_count) / (uk_joy_count + uk_anger_count + uk_fear_count + uk_sadness_count + uk_confident_count) * 100
 
+    //gets trend list for UK
     trend_list = await get_trend(23424975)
 
     uk_score.trend1 = trend_list[0]
@@ -237,7 +267,7 @@ async function analyse_tweets() {
     uk_score.trend4 = trend_list[3]
     uk_score.trend5 = trend_list[4]
 
-
+    //gets list of happiest cities
     happiest_cities = get_happiest(region_analysis)
 
     uk_score.happiest1 = happiest_cities[0].name
@@ -246,12 +276,14 @@ async function analyse_tweets() {
     uk_score.happiest4 = happiest_cities[3].name
     uk_score.happiest5 = happiest_cities[4].name
 
+    // adds uk analysis to analysis list
     region_analysis.push(uk_score)
 
     return region_analysis
 
 }
 
+//uploads analysis to MongoDB
 async function upload_analysis(db_upload) {
 
     try {
@@ -261,7 +293,6 @@ async function upload_analysis(db_upload) {
         const database = client.db("emotional-map");
         const collection = database.collection("emotion-data");
         // create a document to be inserted
-        //const result = await collection.insertOne(example_json);
         const result = await collection.insertOne({ _id: Date.now(), db_upload });
 
         console.log(
@@ -276,18 +307,14 @@ async function upload_analysis(db_upload) {
 
 async function main() {
 
+    //resets data list
     data_list = []
     await get_region_tweets()
     await analyse_tweets()
 
-    console.log(region_analysis)
-
     db_upload = { "anlysis": region_analysis }
-
-    console.log(db_upload)
     await upload_analysis(db_upload)
 }
-
 
 app.get('/api/recent', async (req, res) => {
 
@@ -295,18 +322,20 @@ app.get('/api/recent', async (req, res) => {
     const collection = database.collection("emotion-data");
     // finds most recent database entry
     collection.find().sort({ "_id": -1 }).limit(1).next()
-    .then(results => {
-        // returns data stored in entry
-        res.status(200).send(results.db_upload)
-    }).catch(error => {
-        console.error(error)
-        res.status(500).send(`Server Error: Can't access most recent data`)
-    })
+        .then(results => {
+            // returns data stored in entry
+            res.status(200).send(results.db_upload)
+        }).catch(error => {
+            console.error(error)
+            res.status(500).send(`Server Error: Can't access most recent data`)
+        })
 })
 
 client.connect();
-// main()
-// setInterval(function(){main()}, process.env.DATA_REFRESH_INTERVAL)
+
+//runs analysis on launch and periodically thereafter
+main()
+setInterval(function () { main() }, process.env.DATA_REFRESH_INTERVAL)
 
 const port = process.env.PORT || 8020
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`))
